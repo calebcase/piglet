5453179c68f9406c85206623e1856248225bec1c
Caleb Case <calebcase@gmail.com>
2013-04-09 20:28:17 -0400

    It Begins

--- README.md
+++ README.md
@@ -2,3 +2,95 @@ piglet
 ======
 
 Your friendly little restful time series service.
+
+The Proposal
+------------
+
+- /:id/:type
+  - POST
+    - Request => Data is a JSON number. Time will be the current time on the server.
+
+- /:id/:type/:start
+  - GET
+    - Response =>
+      Range is from the given start till current time.
+
+      ?format=json: key is time, value is a number.
+      ?format=<jpeg|png|svg>: Rendered graph of data.
+    - Parameters =>
+      format=<json|jpeg|png|svg> (defaults to json)
+      height=<px> (defaults to 480)
+      width=<px> (defaults to 640)
+      slice=<timespec> (see below) (defaults to 1 minute)
+      aggregate=<none|count|sum|mean|median|max|min|Q1|Q2|Q3> (defaults to mean)
+      merge=<types> (defaults to none)
+  - POST
+    - Request => Data is a JSON number. Time is set to the value in the url.
+
+- /:id/:type/:start/:stop
+  - Same as above but:
+    Range is from the given start to stop time.
+
+Alternation in the URL can be used to select multiple ids and types at a time:
+
+```
+/vm/{cpu,ram,io}/1365472770/1365473070
+```
+
+Time can be in a convient format like that accepted by 'at' (aka timespec):
+
+```
+/vm/{cpu,ram,io}/last monday/last wednesday
+/vm/{cpu,ram,io}/last month
+```
+
+All of a given type can be selected with '*':
+
+```
+/vm/*/yesterday
+```
+
+With a limited form of globbing we could also permit matches on a subset of types:
+
+```
+vm/net.*,io.sda{1,2}/last week
+```
+
+That would permit a very nice 'subtype' style where you could have several types for network or io, but also easily select all of them. If merge is specified, then the subtypes under the supplied type (e.g. merge=net) would be recombined into a pseudo-type (e.g. All of the net.* values would be seen as just generic 'net' values).
+
+If a slice is specified, the data is aggregated into fixed size chunks. The midpoint is the time reported for the slice. The aggregation method specifies how the data in a given slice should be combined into a single datapoint.
+
+Responses with JSON will always be canonical/rooted:
+
+```json
+{
+  "vm": {
+    "cpu": {
+      "1365472770": 0.20
+      ...
+    },
+    "ram": {...}
+    "io": {...}
+  }
+}
+```
+
+In this way it is consistent for the user to parse responses where they have requested multiple series.
+
+```
+/vm{1,2,3}/cpu,ram/yesterday?slice=10m
+```
+
+```json
+{
+  "vm1": {
+    "cpu": {
+      "1365472770": 0.20
+      ...
+    },
+    "ram": {...}
+  },
+  "vm2": {...},
+  "vm3": {...}
+}
+```
---
+++ app.js
@@ -0,0 +1,359 @@
+/* Copyright 2013 Caleb Case
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var connect = require('connect');
+var express = require('express');
+var resourceful = require('resourceful');
+var timespec = require('timespec');
+
+var MemoryStore = connect.session.MemoryStore;
+
+function date(str) {
+  var at = NaN;
+  var perr = null;
+
+  /* Try a timespec. */
+  try {
+    at = timespec.parse(str);
+  }
+  catch(exc) {
+    perr = exc;
+  }
+
+  /* Try converting to an integer. */
+  if (isNaN(at)) {
+    at = new Date(parseInt(str));
+  }
+
+  /* Try the native date parser. */
+  if (isNaN(at)) {
+    at = new Date(str);
+  }
+
+  /* We've done our best... report failure. */
+  if (isNaN(at)) {
+    throw {'input': str, 'errors': {'timespec': perr, 'as integer': 'NaN', 'as string': 'NaN'}};
+  }
+
+  return at;
+}
+
+var app = express();
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({
+  'secret': 'foo',
+  'store': new MemoryStore({
+    'reapInterval': 60000 * 60
+  })
+}));
+
+/* Data Types */
+resourceful.use('couchdb', {
+  'database': 'timeseries'
+});
+
+var ID = resourceful.define('id', function() {
+  this.string('type');
+  this.string('id');
+});
+
+var Type = resourceful.define('type', function() {
+  this.string('type');
+  this.string('id');
+  this.string('parent');
+});
+
+var Data = resourceful.define('data', function() {
+  this.string('type');
+  this.string('id');
+  this.string('parent');
+  this.number('date');
+  this.number('value');
+});
+
+/* LOGIN */
+
+app.get('/login', function(req, res) {
+  res.type('application/json');
+  res.send(JSON.stringify({}, null, 2));
+});
+
+app.post('/login', function(req, res) {
+  res.type('application/json');
+  res.send(JSON.stringify({}, null, 2));
+});
+
+app.delete('/login', function(req, res) {
+  res.type('application/json');
+  res.send(JSON.stringify({}, null, 2));
+});
+
+/* SETTINGS */
+
+app.get('/settings', function(req, res) {
+  res.type('application/json');
+  res.send(JSON.stringify({}, null, 2));
+});
+
+app.post('/settings', function(req, res) {
+  res.type('application/json');
+  res.send(JSON.stringify({}, null, 2));
+});
+
+/* SERIES */
+
+//app.get('/series', function(req, res) {
+//  res.send(JSON.stringify({}, null, 2));
+//});
+
+app.get('/series/:id', function(req, res) {
+  ID.get(req.params.id, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    res.type('application/json');
+    res.send(JSON.stringify(result, null, 2));
+  });
+});
+
+app.post('/series/:id', function(req, res) {
+  ID.create({
+    'type': 'id',
+    'id': req.params.id
+  }, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    res.type('application/json');
+    res.send(JSON.stringify(result, null, 2));
+  });
+});
+
+app.get('/series/:id/:type', function(req, res) {
+  ID.get(req.params.id, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
+      if (err !== null) {
+        res.type('application/json');
+        res.send(400, JSON.stringify(err, null, 2));
+        return;
+      }
+
+      res.type('application/json');
+      res.send(JSON.stringify(result, null, 2));
+    });
+  });
+});
+
+app.post('/series/:id/:type', function(req, res) {
+  ID.get(req.params.id, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    Type.create({
+      'type': 'type',
+      'id': [req.params.id, req.params.type].join('/'),
+      'parent': req.params.id
+    }, function(err, result) {
+      if (err !== null && err['error'] !== 'conflict') {
+        res.type('application/json');
+        res.send(400, JSON.stringify(err, null, 2));
+        return;
+      }
+
+
+      var now = new Date();
+      Data.create({
+        'type': 'data',
+        'id': [req.params.id, req.params.type, now.getTime()].join('/'),
+        'parent': [req.params.id, req.params.type].join('/'),
+        'date': now.getTime(),
+        'value': req.body.data
+      }, function(err, result) {
+        if (err !== null) {
+          res.type('application/json');
+          res.send(400, JSON.stringify(err, null, 2));
+          return;
+        }
+
+        res.type('application/json');
+        res.send(JSON.stringify(result, null, 2));
+      });
+    });
+  });
+});
+
+app.get('/series/:id/:type/:start', function(req, res) {
+  ID.get(req.params.id, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
+      if (err !== null) {
+        res.type('application/json');
+        res.send(400, JSON.stringify(err, null, 2));
+        return;
+      }
+
+      Data.find({
+        'type': 'data',
+        'parent': [req.params.id, req.params.type].join('/')
+      }, function(err, result) {
+        if (err !== null) {
+          res.type('application/json');
+          res.send(400, JSON.stringif(err, null, 2));
+          return;
+        }
+
+        try {
+          var since = date(req.params.start);
+        }
+        catch(exc) {
+          res.type('application/json');
+          res.send(400, JSON.stringify(exc, null, 2));
+          return;
+        }
+
+        var found = {};
+        for (var i = 0; i < result.length; i++) {
+          var data = result[i];
+          if (data.date >= since.getTime()) {
+            found[data.date] = data.value;
+          }
+        }
+
+        res.type('application/json');
+        res.send(JSON.stringify(found, null, 2));
+      });
+    });
+  });
+});
+
+app.post('/series/:id/:type/:start', function(req, res) {
+  ID.get(req.params.id, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
+      if (err !== null) {
+        res.type('application/json');
+        res.send(400, JSON.stringify(err, null, 2));
+        return;
+      }
+
+
+      try {
+        var at = date(req.params.start);
+      }
+      catch(exc) {
+        res.type('application/json');
+        res.send(400, JSON.stringify(exc, null, 2));
+        return;
+      }
+
+      Data.create({
+        'type': 'data',
+        'id': [req.params.id, req.params.type, at.getTime()].join('/'),
+        'parent': [req.params.id, req.params.type].join('/'),
+        'date': at.getTime(),
+        'value': req.body.data
+      }, function(err, result) {
+        if (err !== null) {
+          res.type('application/json');
+          res.send(400, JSON.stringify(err, null, 2));
+          return;
+        }
+
+        res.type('application/json');
+        res.send(JSON.stringify(result, null, 2));
+      });
+    });
+  });
+});
+
+app.get('/series/:id/:type/:start/:stop', function(req, res) {
+  ID.get(req.params.id, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
+      if (err !== null) {
+        res.type('application/json');
+        res.send(400, JSON.stringify(err, null, 2));
+        return;
+      }
+
+      Data.find({
+        'type': 'data',
+        'parent': [req.params.id, req.params.type].join('/')
+      }, function(err, result) {
+        if (err !== null) {
+          res.type('application/json');
+          res.send(400, JSON.stringif(err, null, 2));
+          return;
+        }
+
+        try {
+          var since = date(req.params.start);
+          var until = date(req.params.stop);
+        }
+        catch(exc) {
+          res.type('application/json');
+          res.send(400, JSON.stringify(exc, null, 2));
+          return;
+        }
+
+        var found = {};
+        for (var i = 0; i < result.length; i++) {
+          var data = result[i];
+          if (data.date >= since.getTime() &&
+              date.date <= until.getTime()) {
+            found[data.date] = data.value;
+          }
+        }
+
+        res.type('application/json');
+        res.send(JSON.stringify(found, null, 2));
+      });
+    });
+  });
+});
+
+console.log('Listening on 3030.');
+app.listen(3030);
