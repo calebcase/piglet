841bdfd775ae6c402650f17f20782cb9b5eca3c2
Caleb Case <calebcase@gmail.com>
2013-04-10 23:49:10 -0400

    MongoDB and Globbing
    Don't ask... it's a long story. For now we'll go with a single backend.
    Once the needs of the backend are clear we can reopen the discussion of
    multiple backends.

--- app.js
+++ app.js
@@ -15,10 +15,18 @@
 
 var connect = require('connect');
 var express = require('express');
-var resourceful = require('resourceful');
+var minimatch = require('minimatch');
+var mongoose = require('mongoose');
 var timespec = require('timespec');
 
 var MemoryStore = connect.session.MemoryStore;
+
+var minimatch_options = {
+  'noglobstar': true,
+  'dot': true,
+  'noext': true,
+  'nocomment': true
+};
 
 function date(str) {
   var at = NaN;
@@ -61,28 +69,35 @@ app.use(express.session({
 }));
 
 /* Data Types */
-resourceful.use('couchdb', {
-  'database': 'piglet'
+
+mongoose.connect('localhost', 'piglet');
+
+var Schema = mongoose.Schema;
+
+var idSchema = new Schema({
+  '_id': { 'type': String },
+  'type': { 'type': String, 'default': 'id' },
 });
 
-var ID = resourceful.define('id', function() {
-  this.string('type');
-  this.string('id');
+var ID = mongoose.model('ID', idSchema);
+
+var typeSchema = new Schema({
+  '_id': { 'type': String },
+  'type': { 'type': String, 'default': 'type' },
+  'parent': { 'type': String, 'ref': 'ID' },
 });
 
-var Type = resourceful.define('type', function() {
-  this.string('type');
-  this.string('id');
-  this.string('parent');
+var Type = mongoose.model('Type', typeSchema);
+
+var dataSchema = new Schema({
+  '_id': { 'type': String },
+  'type': { 'type': String, 'default': 'data' },
+  'parent': { 'type': String, 'ref': 'Type' },
+  'date': { 'type': Date, 'default': Date.now },
+  'value': { 'type': Number },
 });
 
-var Data = resourceful.define('data', function() {
-  this.string('type');
-  this.string('id');
-  this.string('parent');
-  this.number('date');
-  this.number('value');
-});
+var Data = mongoose.model('Data', dataSchema);
 
 /* LOGIN */
 
@@ -115,293 +130,300 @@ app.post('/settings', function(req, res) {
 
 /* SERIES */
 
-//app.get('/series', function(req, res) {
-//  res.send(JSON.stringify({}, null, 2));
-//});
-
-app.get('/series/:id', function(req, res) {
-  ID.get(req.params.id, function(err, result) {
+app.get('/series', function(req, res) {
+  ID.find(function(err, result) {
     if (err !== null) {
       res.type('application/json');
       res.send(400, JSON.stringify(err, null, 2));
       return;
     }
 
+    var data = { 'series': {} };
+    for (var i = 0; i < result.length; i++) {
+      data.series[result[i]._id] = null;
+    }
+
     res.type('application/json');
-    data = {'series': {}};
-    data.series[req.params.id] = null;
+    res.send(JSON.stringify(data, null, 2));
+  });
+});
+
+app.get('/series/:id', function(req, res) {
+  var search = new minimatch.Minimatch(req.params.id, minimatch_options).makeRe();
+  if (!search) {
+    res.type('application/json');
+    res.send(400, JSON.stringify({ 'message': 'Invalid glob specified.' }, null, 2));
+  }
+
+  Type.find({ 'parent': search }, function(err, result) {
+    if (err !== null) {
+      res.type('application/json');
+      res.send(400, JSON.stringify(err, null, 2));
+      return;
+    }
+
+    var data = { 'series': {} };
+    for (var i = 0; i < result.length; i++) {
+      if (!(result[i].parent in data.series)) {
+        data.series[result[i].parent] = {};
+      }
+
+      data.series[result[i].parent][result[i]._id.split('/')[1]] = null;
+    }
+
+    res.type('application/json');
     res.send(JSON.stringify(data, null, 2));
   });
 });
 
 app.post('/series/:id', function(req, res) {
-  ID.create({
-    'type': 'id',
-    'id': req.params.id
-  }, function(err, result) {
+  var instance = new ID();
+  instance._id = req.params.id;
+  instance.save(function(err) {
     if (err !== null) {
       res.type('application/json');
       res.send(400, JSON.stringify(err, null, 2));
       return;
     }
 
-    res.type('application/json');
-    data = {'series': {}};
+    data = { 'series': {} };
     data.series[req.params.id] = null;
+
+    res.type('application/json');
     res.send(JSON.stringify(data, null, 2));
   });
 });
 
-app.get('/series/:id/:type', function(req, res) {
-  ID.get(req.params.id, function(err, result) {
+app.delete('/series/:id', function(req, res) {
+  var search = new minimatch.Minimatch(req.params.id + '/*', minimatch_options).makeRe();
+  if (!search) {
+    res.type('application/json');
+    res.send(400, JSON.stringify({ 'message': 'Invalid glob specified.' }, null, 2));
+  }
+
+  Data.find({ 'parent': search }).remove();
+
+  var search = new minimatch.Minimatch(req.params.id, minimatch_options).makeRe();
+  if (!search) {
+    res.type('application/json');
+    res.send(400, JSON.stringify({ 'message': 'Invalid glob specified.' }, null, 2));
+  }
+
+  Type.find({ 'parent': search }).remove();
+  ID.find({ '_id': search }).remove();
+
+  res.type('application/json');
+  res.send(JSON.stringify({ 'status': 'ok' }, null, 2));
+});
+
+app.get(/\/series\/(([^\/]+)\/([^\/]+))(?:\/([^\/]+)(?:\/([^\/]+))?)?/, function(req, res) {
+  req.params.path = req.params[0];
+  req.params.id = req.params[1];
+  req.params.type = req.params[2];
+
+  req.params.start = req.params[3];
+  req.params.stop = req.params[4];
+
+  var search = new minimatch.Minimatch(req.params.path, minimatch_options).makeRe();
+  if (!search) {
+    res.type('application/json');
+    res.send(400, JSON.stringify({ 'message': 'Invalid glob specified.' }, null, 2));
+  }
+
+  var cb = function (err, result) {
     if (err !== null) {
       res.type('application/json');
       res.send(400, JSON.stringify(err, null, 2));
       return;
     }
 
-    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
-      if (err !== null) {
-        res.type('application/json');
-        res.send(400, JSON.stringify(err, null, 2));
-        return;
+    var data = { 'series': {} };
+
+    for (var i = 0; i < result.length; i++) {
+      var segments = result[i].parent.split('/');
+      var id = segments[0];
+      var type = segments[1];
+
+      if (!(id in data.series)) {
+        data.series[id] = {};
       }
 
-      Data.find({
-        'type': 'data',
-        'parent': [req.params.id, req.params.type].join('/')
-      }, function(err, result) {
-        if (err !== null) {
-          res.type('application/json');
-          res.send(400, JSON.stringify(err, null, 2));
-          return;
-        }
+      if (!(type in data.series[id])) {
+        data.series[id][type] = {};
+      }
 
-        var found = {};
-        for (var i = 0; i < result.length; i++) {
-          var data = result[i];
-          found[data.date] = data.value;
-        }
+      data.series[id][type][result[i].date.getTime()] = result[i].value;
+    }
 
-        res.type('application/json');
-        data = {'series': {}};
-        data.series[req.params.id] = {};
-        data.series[req.params.id][req.params.type] = found;
-        res.send(JSON.stringify(found, null, 2));
-      });
-    });
-  });
+    res.type('application/json');
+    res.send(JSON.stringify(data, null, 2));
+  };
+
+  if (!req.params.start && !req.params.stop) {
+    Data.find({
+      'parent': search
+    }, cb);
+  }
+  else if (req.params.start && !req.params.stop) {
+    var start = date(req.params.start);
+
+    Data.find({
+      'parent': search
+    })
+    .where('date')
+    .gte(start)
+    .exec(cb);
+  }
+  else if (req.params.start && req.params.stop) {
+    var start = date(req.params.start);
+    var stop = date(req.params.stop);
+
+    Data.find({
+      'parent': search
+    })
+    .where('date')
+    .gte(start)
+    .lte(stop)
+    .exec(cb);
+  }
+  else {
+    /* Should never happen... */
+    res.type('application/json');
+    res.send(500, JSON.stringify({ 'message': 'Aww... Now we\'ll never have bacon.' }, null, 2));
+  }
 });
 
-app.post('/series/:id/:type', function(req, res) {
-  if (typeof(req.body.data) === 'undefined') {
+app.post(/\/series\/(([^\/]+)\/([^\/]+))(?:\/([^\/]+))?/, function(req, res) {
+  req.params.path = req.params[0];
+  req.params.id = req.params[1];
+  req.params.type = req.params[2];
+
+  req.params.start = req.params[3];
+
+  if (typeof(req.body.value) === 'undefined') {
     res.type('application/json');
-    res.send(400, JSON.stringify({'message': 'Request body could not be parsed. Headers set?'}, null, 2));
+    res.send(400, JSON.stringify({ 'message': 'Request body could not be parsed. Headers set?'}, null, 2));
     return;
   }
 
-  ID.get(req.params.id, function(err, result) {
+  Type.find({ '_id': req.params.path }, function(err, result) {
     if (err !== null) {
       res.type('application/json');
       res.send(400, JSON.stringify(err, null, 2));
       return;
     }
 
-    Type.create({
-      'type': 'type',
-      'id': [req.params.id, req.params.type].join('/'),
-      'parent': req.params.id
-    }, function(err, result) {
-      if (err !== null && err['error'] !== 'conflict') {
-        res.type('application/json');
-        res.send(400, JSON.stringify(err, null, 2));
-        return;
-      }
-
-      var now = new Date();
-      Data.create({
-        'type': 'data',
-        'id': [req.params.id, req.params.type, now.getTime()].join('/'),
-        'parent': [req.params.id, req.params.type].join('/'),
-        'date': now.getTime(),
-        'value': req.body.data
-      }, function(err, result) {
+    if (result.length === 0) {
+      var instance = new Type();
+      instance._id = req.params.path;
+      instance.parent = req.params.id;
+      instance.save(function(err) {
         if (err !== null) {
           res.type('application/json');
           res.send(400, JSON.stringify(err, null, 2));
           return;
         }
-
-        res.type('application/json');
-        data = {'series': {}};
-        data.series[req.params.id] = {};
-        data.series[req.params.id][req.params.type] = {};
-        data.series[req.params.id][req.params.type][result.date] = result.value;
-        res.send(JSON.stringify(data, null, 2));
       });
-    });
-  });
-});
-
-app.get('/series/:id/:type/:start', function(req, res) {
-  ID.get(req.params.id, function(err, result) {
-    if (err !== null) {
-      res.type('application/json');
-      res.send(400, JSON.stringify(err, null, 2));
-      return;
     }
 
-    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
+    var now = new Date();
+    if (req.params.start) {
+      now = date(req.params.start);
+    }
+
+    var instance = new Data();
+    instance._id = [req.params.path, now.getTime()];
+    instance.parent = req.params.path;
+    instance.date = now;
+    instance.value = req.body.value;
+    instance.save(function(err) {
       if (err !== null) {
         res.type('application/json');
         res.send(400, JSON.stringify(err, null, 2));
         return;
       }
 
-      Data.find({
-        'type': 'data',
-        'parent': [req.params.id, req.params.type].join('/')
-      }, function(err, result) {
-        if (err !== null) {
-          res.type('application/json');
-          res.send(400, JSON.stringify(err, null, 2));
-          return;
-        }
+      data = { 'series': {} };
+      data.series[req.params.id] = {};
+      data.series[req.params.id][req.params.type] = {};
+      data.series[req.params.id][req.params.type][now.getTime()] = req.body.value;
 
-        try {
-          var since = date(req.params.start);
-        }
-        catch(exc) {
-          res.type('application/json');
-          res.send(400, JSON.stringify(exc, null, 2));
-          return;
-        }
-
-        var found = {};
-        for (var i = 0; i < result.length; i++) {
-          var data = result[i];
-          if (data.date >= since.getTime()) {
-            found[data.date] = data.value;
-          }
-        }
-
-        res.type('application/json');
-        data = {'series': {}};
-        data.series[req.params.id] = {};
-        data.series[req.params.id][req.params.type] = found;
-        res.send(JSON.stringify(data, null, 2));
-      });
+      res.type('application/json');
+      res.send(JSON.stringify(data, null, 2));
     });
   });
 });
 
-app.post('/series/:id/:type/:start', function(req, res) {
-  if (typeof(req.body.data) === 'undefined') {
+app.delete(/\/series\/(([^\/]+)\/([^\/]+))/, function(req, res) {
+  req.params.path = req.params[0];
+  req.params.id = req.params[1];
+  req.params.type = req.params[2];
+
+  var search = new minimatch.Minimatch(req.params.path, minimatch_options).makeRe();
+  if (!search) {
     res.type('application/json');
-    res.send(400, JSON.stringify({'message': 'Request body could not be parsed. Headers set?'}, null, 2));
-    return;
+    res.send(400, JSON.stringify({ 'message': 'Invalid glob specified.' }, null, 2));
   }
 
-  ID.get(req.params.id, function(err, result) {
-    if (err !== null) {
-      res.type('application/json');
-      res.send(400, JSON.stringify(err, null, 2));
-      return;
-    }
+  Type.find({
+    '_id': search
+  })
+  .remove();
 
-    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
-      if (err !== null) {
-        res.type('application/json');
-        res.send(400, JSON.stringify(err, null, 2));
-        return;
-      }
-
-
-      try {
-        var at = date(req.params.start);
-      }
-      catch(exc) {
-        res.type('application/json');
-        res.send(400, JSON.stringify(exc, null, 2));
-        return;
-      }
-
-      Data.create({
-        'type': 'data',
-        'id': [req.params.id, req.params.type, at.getTime()].join('/'),
-        'parent': [req.params.id, req.params.type].join('/'),
-        'date': at.getTime(),
-        'value': req.body.data
-      }, function(err, result) {
-        if (err !== null) {
-          res.type('application/json');
-          res.send(400, JSON.stringify(err, null, 2));
-          return;
-        }
-
-        res.type('application/json');
-        data = {'series': {}};
-        data.series[req.params.id] = {};
-        data.series[req.params.id][req.params.type] = {};
-        data.series[req.params.id][req.params.type][result.date] = result.value;
-        res.send(JSON.stringify(data, null, 2));
-      });
-    });
-  });
+  res.type('application/json');
+  res.send(JSON.stringify({ 'status': 'ok' }, null, 2));
 });
 
-app.get('/series/:id/:type/:start/:stop', function(req, res) {
-  ID.get(req.params.id, function(err, result) {
-    if (err !== null) {
-      res.type('application/json');
-      res.send(400, JSON.stringify(err, null, 2));
-      return;
-    }
+app.delete(/\/series\/(([^\/]+)\/([^\/]+))\/(?:([^\/]+)(?:\/([^\/]+))?)?/, function(req, res) {
+  req.params.path = req.params[0];
+  req.params.id = req.params[1];
+  req.params.type = req.params[2];
 
-    Type.get([req.params.id, req.params.type].join('/'), function(err, result) {
-      if (err !== null) {
-        res.type('application/json');
-        res.send(400, JSON.stringify(err, null, 2));
-        return;
-      }
+  req.params.start = req.params[3];
+  req.params.stop = req.params[4];
 
-      Data.find({
-        'type': 'data',
-        'parent': [req.params.id, req.params.type].join('/')
-      }, function(err, result) {
-        if (err !== null) {
-          res.type('application/json');
-          res.send(400, JSON.stringify(err, null, 2));
-          return;
-        }
+  var search = new minimatch.Minimatch(req.params.path, minimatch_options).makeRe();
+  if (!search) {
+    res.type('application/json');
+    res.send(400, JSON.stringify({ 'message': 'Invalid glob specified.' }, null, 2));
+  }
 
-        try {
-          var since = date(req.params.start);
-          var until = date(req.params.stop);
-        }
-        catch(exc) {
-          res.type('application/json');
-          res.send(400, JSON.stringify(exc, null, 2));
-          return;
-        }
+  if (!req.params.start && !req.params.stop) {
+    Data.find({
+      'parent': search
+    })
+    .remove();
+  }
+  else if (req.params.start && !req.params.stop) {
+    var start = date(req.params.start);
 
-        var found = {};
-        for (var i = 0; i < result.length; i++) {
-          var data = result[i];
-          if (data.date >= since.getTime() &&
-              data.date <= until.getTime()) {
-            found[data.date] = data.value;
-          }
-        }
+    Data.find({
+      'parent': search
+    })
+    .where('date')
+    .gte(start)
+    .remove();
+  }
+  else if (req.params.start && req.params.stop) {
+    var start = date(req.params.start);
+    var stop = date(req.params.stop);
 
-        res.type('application/json');
-        data = {'series': {}};
-        data.series[req.params.id] = {};
-        data.series[req.params.id][req.params.type] = found;
-        res.send(JSON.stringify(data, null, 2));
-      });
-    });
-  });
+    Data.find({
+      'parent': search
+    })
+    .where('date')
+    .gte(start)
+    .lte(stop)
+    .remove();
+  }
+  else {
+    /* Should never happen... */
+    res.type('application/json');
+    res.send(500, JSON.stringify({ 'message': 'Aww... Now we\'ll never have bacon.' }, null, 2));
+  }
+
+  res.type('application/json');
+  res.send(JSON.stringify({ 'status': 'ok' }, null, 2));
 });
 
 console.log('Listening on 3030.');
